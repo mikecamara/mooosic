ffeda594abbc0205df814f75991fdac46f4e12b2
Mike Camara <m.camara@vbreathe.com>
2023-04-22 15:49:08 +1000

    (feat) The background of the media controls should blur the songs list behind it

--- src/components/MediaPlayer/MediaPlayer.tsx
+++ src/components/MediaPlayer/MediaPlayer.tsx
@@ -1,3 +1,4 @@
+import { BlurView } from 'expo-blur';
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import type Song from '../../types/Song.ts';
@@ -10,18 +11,36 @@ const styles = StyleSheet.create({
   artist: {
     color: ORANGE_COLOR,
   },
+  blurView: {
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: -10, // Move the blurView up by 10 units
+  },
   container: {
     alignItems: 'center',
-    backgroundColor: LIGHT_GRAY_COLOR,
     borderColor: GRAY_COLOR,
     borderTopWidth: 1,
     bottom: 0,
     flexDirection: 'row',
-    height: 60,
+    height: 70, // Increase the height to 70
     justifyContent: 'space-between',
     left: 0,
-    opacity: 0.95,
+    paddingBottom: 30,
     paddingHorizontal: 16,
+    position: 'absolute',
+    right: 0,
+  },
+  content: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  mediaPlayerWrapper: {
+    bottom: 0,
+    left: 0,
     position: 'absolute',
     right: 0,
   },
@@ -66,14 +85,23 @@ function MediaPlayer({
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.songInfo}>
-        <Text style={styles.title}>{currentSong.title}</Text>
-        <Text style={styles.artist}>{currentSong.artist}</Text>
+    <View style={styles.mediaPlayerWrapper}>
+      <View style={styles.container}>
+        <BlurView
+          intensity={50}
+          style={styles.blurView}
+          tint="light" // Add this line
+        />
+        <View style={styles.content}>
+          <View style={styles.songInfo}>
+            <Text style={styles.title}>{currentSong.title}</Text>
+            <Text style={styles.artist}>{currentSong.artist}</Text>
+          </View>
+          <TouchableOpacity style={styles.playButton} onPress={handlePlayPause}>
+            <Text style={styles.playButtonText}>{isPlaying ? '⏸️' : '▶️'}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <TouchableOpacity style={styles.playButton} onPress={handlePlayPause}>
-        <Text style={styles.playButtonText}>{isPlaying ? '⏸️' : '▶️'}</Text>
-      </TouchableOpacity>
     </View>
   );
 }
